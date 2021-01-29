a992ecb2f91b8e43d631c558a279d5dc6e159023
RuslanSalimov <mail-spam2012@yandex.ru>
2021-01-29 23:39:02 +0500

    Переделал

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,15 @@
-//Создать переменную num со значением 266219 (тип данных число)
 let num = 266219;
-let numToArray = numberToArray(num);
-let sumOfDigits = summArrayItems(numToArray);
 
-// Вывести в консоль произведение (умножение) цифр этого числа
-console.log(sumOfDigits);
+let strNum = String(num);
 
-// Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
-let secondNumber = sumOfDigits ** 3;
-//Вывести на экран первые 2 цифры полученного числа
-let secondArray = numberToArray(secondNumber);
-alert(summArrayItems(secondArray, 0, 2));
-
-
-
-/*------------- functions ---------------*/
-/**
- * Функция создания массива из цифр переданного числа
- * @param number - число для создания массива
- * @returns Array() если аргумент число, иначе -1
- */
-function numberToArray(number) {
-    // может по ошибке попалась строка?
-    number = +number;
-    // если +number == Nan, то условие не выполнится
-    if(number){
-        let arr = [];
-        while(number >= 1){
-            arr.push(number%10);    //забираем в массив последнюю цифру
-            //number = Math.floor(number/10); //"обрезаем" число
-            number = number / 10 ^ 0; //"обрезаем" число
-        }
-        //----разворачиваем
-        return arr.reverse();
-    }
-    return -1;
+let multNum = 1;
+for (let i = 0; i < strNum.length; i++){
+    multNum *= strNum[i];
 }
 
-/**
- * Функция суммирования числовых элементов массива от стартового индекса на
- * необходимое число элементов, имеет защиту от выхода за пределы, если параметры начала счёта и
- * числа элементов не заданы, считает весь массив
- * @param arr - исходный массив
- * @param start - начальный индекс
- * @param length - сколько членов массива суммировать от начального индекса
- * @returns {number} - сумма, если arr - массив, иначе -1
- */
-function summArrayItems(arr, start = 0, length = arr.length) {
-    if(Array.isArray(arr)){
-        //-------- Защита от выхода за пределы --------------------//
-        //если start больше длины или отрицателен, то будем считать сначала
-        start = (start < arr.length - 1) && (start > 0) ? start : 0;
-        //если (start + length) больше последнего индекса, то length равно длина массива минус start
-        length = ( (start + length)  < arr.length - 1) ? length : arr.length - start;
+console.log(multNum)
 
-        //------- считаем ------------
-        let summ = 0;
-        //----      i-счетчик шагов, j-счетчик по элементам массива
-        for(let i = 0, j = start; i < length; i++, j++){
-            summ += (+arr[j]) ? arr[j] : 0;
-        }
-        return summ;
-    }
-    return -1;
-}
-
+multNum = multNum**3;
+console.log(multNum);
+let strMultNum = String(multNum);
+console.log('первое число: ' + strMultNum[0] + "    " + 'второе число: ' + strMultNum[1]);
