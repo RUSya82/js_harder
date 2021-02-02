1db278c5019b3c4938f20625be0ce46f29466389
RuslanSalimov <mail-spam2012@yandex.ru>
2021-02-02 20:10:01 +0500

    lesson4 done

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,11 @@
-/*
-    Переменная lang может принимать 2 значения: 'ru' 'en'.
-    Написать условия при котором в зависимости от значения lang
-    будут выводится дни недели на русском или английском языке. Решите задачу
-*/
+function foo(arg) {
+    if(typeof arg !== 'string'){
+        return 'Not a string';
+    }
+    return arg.trim().substr(0, 30) + '...';
 
-let lang = 'ru';
-let daysRU = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-let daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-//через if
-if(lang === 'ru'){
-    console.log(daysRU);
-} else if (lang === 'en'){
-    console.log(daysEn);
-} else {
-    console.log('Неправильно выбран язык');
 }
-
-//через switch-case
-switch (lang) {
-    case "en":
-        console.log(daysEn);
-        break;
-    case "ru":
-        console.log(daysRU);
-        break;
-    default:
-        console.log('Неправильно выбран язык');
-        break;
-}
-
-//через многомерный массив без ифов и switch.
-let days = [daysRU, daysEn];
-console.log((lang === 'ru') ? (days[0]) : (lang === 'en') ? (days[1]) : "Неправильно выбран язык");
-
-/*
-У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
- если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
- */
-
-let namePerson = "Максим";
-console.log((namePerson === "Артем") ? ('Директор') : ((namePerson === 'Максим') ? ('Преподаватель') : ('Студент')));
+let firstStr = '      Однажды в студёную зимнюю пору, я из лесу вышел, и сразу зашёл     ';
+let secondStr = foo(firstStr);
+console.log('Длина строки: ' + secondStr.length);
+console.dir(secondStr);
