b9cd33a51a25ac3fcc6c1c44122f748ad5cfef78
RuslanSalimov <mail-spam2012@yandex.ru>
2021-02-02 13:36:13 +0500

    Выполнил усложненное задание к ургоку №3

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,44 @@
-let num = 266219;
+/*
+    Переменная lang может принимать 2 значения: 'ru' 'en'.
+    Написать условия при котором в зависимости от значения lang
+    будут выводится дни недели на русском или английском языке. Решите задачу
+*/
 
-let strNum = String(num);
+let lang = 'ru';
+let daysRU = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+let daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-let multNum = 1;
-for (let i = 0; i < strNum.length; i++){
-    multNum *= strNum[i];
+//через if
+if(lang === 'ru'){
+    console.log(daysRU);
+} else if (lang === 'en'){
+    console.log(daysEn);
+} else {
+    console.log('Неправильно выбран язык');
 }
 
-console.log(multNum)
+//через switch-case
+switch (lang) {
+    case "en":
+        console.log(daysEn);
+        break;
+    case "ru":
+        console.log(daysRU);
+        break;
+    default:
+        console.log('Неправильно выбран язык');
+        break;
+}
 
-multNum = multNum ** 3;
-console.log(multNum);
-let strMultNum = String(multNum);
-console.log('первое число: ' + strMultNum[0] + "    " + 'второе число: ' + strMultNum[1]);
+//через многомерный массив без ифов и switch.
+let days = [daysRU, daysEn];
+console.log((lang === 'ru') ? (days[0]) : (lang === 'en') ? (days[1]) : "Неправильно выбран язык");
+
+/*
+У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
+ если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
+Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
+ */
+
+let namePerson = "Максим";
+console.log((namePerson === "Артем") ? ('Директор') : ((namePerson === 'Максим') ? ('Преподаватель') : ('Студент')));
