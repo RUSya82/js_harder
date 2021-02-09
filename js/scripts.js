f3ad0cbc0110d170e2e72cfbbb465a2d8a9d932b
RuslanSalimov <mail-spam2012@yandex.ru>
2021-02-10 00:40:56 +0500

    9 lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
 'use strict';
+let container = document.querySelector('.container');
 
-let date = new Date();
 
-let day = date.getDay();
 
-//дни недели
-let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//получим список
-let container = document.querySelector('.container ul');
-//стили курсива
-let coursive = `style="font-style:italic"`;
-//стили жирного шрифта
-let bold = `style="font-weight:bold"`;
-//переменная для текста вывода
-let text = '';
-for (let key in week){
-    text += `<li ${(key == day) ? bold : ''}${( (key == 0) || (key == 6) ) ? coursive :''}>${week[key]}</li>`
+let week = ["Вокресенье", "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+let year = ["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"];
+
+
+
+setInterval(()=>{
+    let date = new Date();
+    let dateA = `Сегодня ${week[date.getDay()]}, ${date.getDate()} ${year[date.getMonth()]} ${date.getFullYear()} года, 
+    ${date.getHours()} ${getHourName(date.getHours())} ${date.getMinutes()} минут ${date.getSeconds()} секунд`;
+    // '04.02.2020 - 21:05:33'
+        let dateB = `${addZero(date.getDate())}.${addZero(date.getMonth())}.${date.getFullYear()} - 
+    ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
+    container.innerHTML = `<h2>${dateA}</h2>`;
+    container.innerHTML += `<h2>${dateB}</h2>`;
+}, 100);
+
+
+
+function getHourName(hour) {
+    if(hour > 4 && hour < 21){
+        return 'часов';
+    }
+    else if(hour%10 === 1){
+        return 'час';
+    }else if((hour%10 > 1) && (hour%10 < 5)){
+        return "часа";
+    }
+    return 'часов';
 }
 
-container.innerHTML = text;
+function addZero(number) {
+    return number > 9 ? number : '0' + number;
+}
